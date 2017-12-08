702e1c4ff99a9f85224d0e0963c8f335a4ea8da4
Amanda Bienz <bienz2@illinois.edu>
2017-12-08 15:05:34 -0600

    Added mkl_spmv

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -12,12 +12,13 @@ find_library(MKL_SEQ_LIBRARY NAMES mkl_sequential
 find_library(MKL_CORE_LIBRARY NAMES mkl_core
         HINTS ${PC_MKL_LIBDIR} ${PC_MKL_LIBRARY_DIRS} 
         ${MKL_DIR}/lib ${MKL_DIR}/lib/intel64_lin)
-find_library(MKL_ILP_LIBRARY NAMES mkl_intel_ilp64
+find_library(MKL_ILP_LIBRARY NAMES mkl_intel_lp64
         HINTS ${PC_MKL_LIBDIR} ${PC_MKL_LIBRARY_DIRS} 
         ${MKL_DIR}/lib ${MKL_DIR}/lib/intel64_lin)
 
 set(MKL_LIBRARIES ${MKL_SEQ_LIBRARY} ${MKL_CORE_LIBRARY} ${MKL_ILP_LIBRARY} )
 set(MKL_INCLUDE_DIRS ${MKL_INCLUDE_DIR} )
+set(MKL_COMPILE_FLAGS "-m64")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Mkl  DEFAULT_MSG
--- raptor/util/linalg/mkl_spmv.cpp
+++ raptor/util/linalg/mkl_spmv.cpp
@@ -10,57 +10,62 @@ using namespace raptor;
 
 void COOMatrix::mult(std::vector<double>& x, std::vector<double>& b)
 {
-    for (int i = 0; i < n_rows; i++)
-        b[i] = 0.0;
-    mult_append(x, b);
+    double alpha = 1.0;
+    double beta = 0.0;
+    mkl_dcoomv("N", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx1.data(), idx2.data(), &nnz, 
+                    x.data(), &beta, b.data());
 }
 void COOMatrix::mult_T(std::vector<double>& x, std::vector<double>& b)
 {
-    for (int i = 0; i < n_cols; i++)
-        b[i] = 0.0;
-
-    mult_append_T(x, b);
+    double alpha = 1.0;
+    double beta = 0.0;
+    mkl_dcoomv("T", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx1.data(), idx2.data(), &nnz, 
+                    x.data(), &beta, b.data());
 }
 void COOMatrix::mult_append(std::vector<double>& x, std::vector<double>& b)
 { 
-    for (int i = 0; i < nnz; i++)
-    {
-        b[idx1[i]] += vals[i] * x[idx2[i]];
-    }
+    double alpha = 1.0;
+    double beta = 1.0;
+    mkl_dcoomv("N", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx1.data(), idx2.data(), &nnz, 
+                    x.data(), &beta, b.data());
 }
 void COOMatrix::mult_append_T(std::vector<double>& x, std::vector<double>& b)
 {
-    for (int i = 0; i < nnz; i++)
-    {
-        b[idx2[i]] += vals[i] * x[idx1[i]];
-    }
+    double alpha = 1.0;
+    double beta = 1.0;
+    mkl_dcoomv("T", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx1.data(), idx2.data(), &nnz, 
+                    x.data(), &beta, b.data());
 }
 void COOMatrix::mult_append_neg(std::vector<double>& x, std::vector<double>& b)
 {
-    for (int i = 0; i < nnz; i++)
-    {
-        b[idx1[i]] -= vals[i] * x[idx2[i]];
-    }
+    double alpha = -1.0;
+    double beta = 1.0;
+    mkl_dcoomv("N", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx1.data(), idx2.data(), &nnz, 
+                    x.data(), &beta, b.data());
 }
 void COOMatrix::mult_append_neg_T(std::vector<double>& x, std::vector<double>& b)
 {
-    for (int i = 0; i < nnz; i++)
-    {
-        b[idx2[i]] -= vals[i] * x[idx1[i]];
-    }
+    double alpha = -1.0;
+    double beta = 1.0;
+    mkl_dcoomv("T", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx1.data(), idx2.data(), &nnz, 
+                    x.data(), &beta, b.data());
 }
 void COOMatrix::residual(const std::vector<double>& x, const std::vector<double>& b, 
         std::vector<double>& r)
 {
-    for (int i = 0; i < n_rows; i++)
-        r[i] = b[i];
-     
-    for (int i = 0; i < nnz; i++)
-    {
-        r[idx1[i]] -= vals[i] * x[idx2[i]];
-    }
+    std::copy(b.begin(), b.end(), r.begin());
+    double alpha = -1.0;
+    double beta = 1.0;
+    mkl_dcoomv("N", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx1.data(), idx2.data(), &nnz, 
+                    x.data(), &beta, r.data());
 }
-
 
 void CSRMatrix::mult(std::vector<double>& x, std::vector<double>& b)
 {
@@ -124,84 +129,61 @@ void CSRMatrix::residual(const std::vector<double>& x, const std::vector<double>
 
 void CSCMatrix::mult(std::vector<double>& x, std::vector<double>& b)
 {
-    for (int i = 0; i < n_rows; i++)
-        b[i] = 0.0;
-    mult_append(x, b);
+    double alpha = 1.0;
+    double beta = 0.0;
+    mkl_dcscmv("N", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx2.data(), &(idx1[0]), &(idx1[1]),  
+                    x.data(), &beta, b.data());
 }
+
 void CSCMatrix::mult_T(std::vector<double>& x, std::vector<double>& b)
 {
-    for (int i = 0; i < n_cols; i++)
-        b[i] = 0.0;
-
-    mult_append_T(x, b);
+    double alpha = 1.0;
+    double beta = 0.0;
+    mkl_dcscmv("T", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx2.data(), &(idx1[0]), &(idx1[1]),  
+                    x.data(), &beta, b.data());
 }
 void CSCMatrix::mult_append(std::vector<double>& x, std::vector<double>& b)
 { 
-    int start, end;
-    for (int i = 0; i < n_cols; i++)
-    {
-        start = idx1[i];
-        end = idx1[i+1];
-        for (int j = start; j < end; j++)
-        {
-            b[idx2[j]] += vals[j] * x[i];
-        }
-    }
+    double alpha = 1.0;
+    double beta = 1.0;
+    mkl_dcscmv("N", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx2.data(), &(idx1[0]), &(idx1[1]),  
+                    x.data(), &beta, b.data());
 }
 void CSCMatrix::mult_append_T(std::vector<double>& x, std::vector<double>& b)
 {
-    int start, end;
-    for (int i = 0; i < n_cols; i++)
-    {
-        start = idx1[i];
-        end = idx1[i+1];
-        for (int j = start; j < end; j++)
-        {
-            b[i] += vals[j] * x[idx2[j]];
-        }
-    }
+    double alpha = 1.0;
+    double beta = 1.0;
+    mkl_dcscmv("T", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx2.data(), &(idx1[0]), &(idx1[1]),  
+                    x.data(), &beta, b.data());
 }
 void CSCMatrix::mult_append_neg(std::vector<double>& x, std::vector<double>& b)
 {
-    int start, end;
-    for (int i = 0; i < n_cols; i++)
-    {
-        start = idx1[i];
-        end = idx1[i+1];
-        for (int j = start; j < end; j++)
-        {
-            b[idx2[j]] -= vals[j] * x[i];
-        }
-    }
+    double alpha = -1.0;
+    double beta = 1.0;
+    mkl_dcscmv("N", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx2.data(), &(idx1[0]), &(idx1[1]),  
+                    x.data(), &beta, b.data());
 }
 void CSCMatrix::mult_append_neg_T(std::vector<double>& x, std::vector<double>& b)
 {
-    int start, end;
-    for (int i = 0; i < n_cols; i++)
-    {
-        start = idx1[i];
-        end = idx1[i+1];
-        for (int j = start; j < end; j++)
-        {
-            b[i] -= vals[j] * x[idx2[j]];
-        }
-    }
+    double alpha = -1.0;
+    double beta = 1.0;
+    mkl_dcscmv("T", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx2.data(), &(idx1[0]), &(idx1[1]),  
+                    x.data(), &beta, b.data());
 }
 void CSCMatrix::residual(const std::vector<double>& x, const std::vector<double>& b, 
         std::vector<double>& r)
 {
-    for (int i = 0; i < n_rows; i++)
-        r[i] = b[i];
-
-    int start, end;
-    for (int i = 0; i < n_cols; i++)
-    {
-        start = idx1[i];
-        end = idx1[i+1];
-        for (int j = start; j < end; j++)
-        {
-            r[idx2[j]] -= vals[j] * x[i];
-        }
-    }
+    std::copy(b.begin(), b.end(), r.begin());
+    double alpha = -1.0;
+    double beta = 1.0;
+    mkl_dcscmv("N", &n_rows, &n_cols, &alpha, "G**C", vals.data(), 
+                    idx2.data(), &(idx1[0]), &(idx1[1]),  
+                    x.data(), &beta, r.data());
 }
 
