c99789fdbf65229b911e6bec077a9d8949b8b1a9
Shelby Lockhart <sll2@dunkel.cs.illinois.edu>
2017-12-08 17:19:58 -0600

    Get stuff working

--- raptor/util/linalg/trisolves.cpp
+++ raptor/util/linalg/trisolves.cpp
@@ -22,6 +22,15 @@ void ParCSRMatrix::fwd_sub(ParVector& y, ParVector& b)
     // Set the values of y equal to the values of b
     y.copy(b);
 
+    int edge = 0;
+    int part = y.global_n/num_procs;
+    if (part*num_procs != y.global_n){
+        edge = y.local_n - part;
+    }
+    MPI_Bcast(&edge, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    //printf("Edge %d\n", edge);
+
     int i, j, start_off, end_off, start_on, end_on;
     // If rank==0, perform seq fwd sub
     if (rank == 0){
@@ -32,12 +41,15 @@ void ParCSRMatrix::fwd_sub(ParVector& y, ParVector& b)
         // Send local updated portion of y to next rank
         MPI_Send(&(y.local)[0], y.local_n, MPI_DOUBLE, rank+1, 1, MPI_COMM_WORLD);
 
+	//printf("Rank %d, sends %d \n", rank, y.local_n);
     }
     // Else receive updated x from previous procs then perform seq fwd sub
     else{
 
-        int recv_cnt = y.local_n * rank;
+        int recv_cnt = y.local_n * rank + edge;
         Vector off_y(recv_cnt);
+
+	//printf("Rank %d, receives %d \n", rank, recv_cnt);
 
         // Receive updated portion of y from previous rank
         MPI_Recv(&(off_y)[0], recv_cnt, MPI_DOUBLE, rank-1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -63,6 +75,9 @@ void ParCSRMatrix::fwd_sub(ParVector& y, ParVector& b)
         if (rank != num_procs-1){
 
             int send_cnt = recv_cnt + y.local_n;
+
+            //printf("Rank %d sends %d \n", rank, send_cnt);
+
             // Appending local y calculations onto off_y calculations
             off_y.values.insert(off_y.values.end(), y.local.values.begin(), y.local.values.end());
             // Send updated portion of y to next rank
---
+++ raptor/util/tests/test_par_fwd_sub.cpp
@@ -0,0 +1,76 @@
+#include <assert.h>
+#include <math.h>
+#include "core/types.hpp"
+#include "core/par_matrix.hpp"
+#include "core/matrix.hpp"
+#include "gallery/par_matrix_IO.hpp"
+#include "gallery/matrix_IO.hpp"
+
+using namespace raptor;
+
+void compare(Vector& b, ParVector& b_par)
+{
+    double b_norm = b.norm(2);
+    double b_par_norm = b_par.norm(2);
+
+    printf("Seq norm: %f\n", b_norm);
+    printf("Par norm: %f\n", b_par_norm);
+
+    assert(fabs(b_norm - b_par_norm) < 1e-06);
+
+    Vector& b_par_lcl = b_par.local;
+    for (int i = 0; i < b_par.local_n; i++)
+    {
+        assert(fabs(b_par_lcl[i] - b[i+b_par.first_local]) < 1e-06);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    MPI_Init(&argc, &argv);
+    int rank, num_procs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    // Read in lower triangular matrix
+    char* fname = "steam1_low.mtx";
+    CSRMatrix* A = readMatrix(fname);
+    ParCSRMatrix* A_par = readParMatrix(fname, MPI_COMM_WORLD, 1, 1);
+
+    Vector x(A->n_rows);
+    Vector b(A->n_rows);
+    b.set_const_value(1.0);
+
+    ParVector x_par(A_par->global_num_cols, A_par->on_proc_num_cols, 
+            A_par->partition->first_local_col);
+    ParVector b_par(A_par->global_num_rows, A_par->local_num_rows, 
+            A_par->partition->first_local_row);
+    b_par.set_const_value(1.0);
+
+    A->fwd_sub(x, b);
+
+    A_par->fwd_sub(x_par, b_par);
+
+    compare(x, x_par);
+    
+    // Set x and x_par to same random values
+    for (int i = 0; i < x.size(); i++)
+    {
+        srand(i);
+        b[i] = ((double)rand()) / RAND_MAX;
+    }
+    for (int i = 0; i < b_par.local_n; i++)
+    {
+        srand(i+b_par.first_local);
+        b_par.local[i] = ((double)rand()) / RAND_MAX;
+    }
+    A->fwd_sub(x, b);
+    A_par->fwd_sub(x_par, b_par);
+    compare(x, x_par);
+
+    delete A;
+    delete A_par;
+
+    MPI_Finalize();
+}
+
